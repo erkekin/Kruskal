88ba1815883dfc705438d4e9c0f5802218d31317
erk ekin <erkekin@gmail.com>
2014-05-31 15:27:23 +0300

    Initial Commit

---
+++ Kruskal/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Kruskal
+//
+//  Created by Erk EKIN on 31/05/14.
+//  Copyright (c) 2014 erkekin. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
